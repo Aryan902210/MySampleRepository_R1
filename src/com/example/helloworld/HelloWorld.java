62010d160892ebe88fd7f45b0768534ae5f4adfc
Aryan Duggal <aryanduggal77@gmail.com>
2025-01-30 14:29:06 -0500

    Initial commit

--- src/com/example/helloworld/HelloWorld.java
+++ src/com/example/helloworld/HelloWorld.java
@@ -5,8 +5,9 @@ public class HelloWorld {
 
     public static void main (String[] args){
 
+          System.out.println("hi");
+          System.out.println("This is my project!");
 
-       
     }
 
 }
